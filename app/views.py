97d49d15cd0cc32066a1d2e0209e8567c5a0de95
h3nr1qu3araujo <henrique.skip@hotmail.com>
2018-11-10 15:04:05 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import Curso
+from app.models import Curso, Vestibular
 from datetime import datetime
 
 def home(request):
@@ -49,16 +49,16 @@ def about(request):
         })
     )
 
-def cadastro_cursos(request):
+def cadastro_vestibulares(request):
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/cadastro_cursos.html',
+        'app/cadastro_vestibulares.html',
         context_instance = RequestContext(request,
         {
-            'title':'Cadastro de cursos',
+            'title':'Cadastro de Vestibulares',
 #            'cursos': ['ADS' , 'SI', 'CC'],
-            'cursos': Curso.objects.all(),
+            'vestibulares': Vestibular.objects.all(),
             'year':datetime.now().year,
         })
     )
